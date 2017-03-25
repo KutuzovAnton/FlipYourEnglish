59c97ed098ed09070d490ed07c13e7efe32f99f0
Anton_Kutuzau <Anton_Kutuzau@epam.com>
2017-03-25 21:36:18 +0300

    Added menu

--- mobile/src/main/java/com/fye/flipyourenglish/MainActivity.java
+++ mobile/src/main/java/com/fye/flipyourenglish/MainActivity.java
@@ -1,7 +1,10 @@
 package com.fye.flipyourenglish;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fye.flipyourenglish.menu.MenuActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    public void init() {
+        findViewById(R.id.main)
+                .getTouchables()
+                .forEach(b -> b.setOnClickListener(new MenuActivity(this)));
+    }
+
 }
--- mobile/src/main/java/com/fye/flipyourenglish/menu/MenuActivity.java
+++ mobile/src/main/java/com/fye/flipyourenglish/menu/MenuActivity.java
@@ -1,43 +1,52 @@
 package com.fye.flipyourenglish.menu;
-
-import android.view.ActionMode;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.fye.flipyourenglish.R;
+import com.fye.flipyourenglish.activities.CardReader;
+import com.fye.flipyourenglish.activities.CardWriter;
+import com.fye.flipyourenglish.entities.Card;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
 
 /**
  * Created by Anton_Kutuzau on 3/13/2017.
  */
 
-public class MenuActivity implements ActionMode.Callback {
+public class MenuActivity implements View.OnClickListener {
 
-    @Override
-    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-        MenuInflater inflater = mode.getMenuInflater();
-        inflater.inflate(R.menu.main_manu, menu);
-        return true;
+    private final static File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/cards/");
+    private final static Gson gson = new GsonBuilder().create();
+    private final Activity activity;
+
+    public MenuActivity(Activity activity) {
+        this.activity = activity;
     }
 
     @Override
-    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-        return false;
-    }
-
-    @Override
-    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.help:
-                mode.finish();
-                return true;
-            default:
-                return false;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.view_cards:
+                activity.startActivity(new Intent(activity, CardReader.class));
+                break;
+            case R.id.add_cards:
+                activity.startActivity(new Intent(activity, CardWriter.class));
+                break;
+            case R.id.delete_cards:
+                break;
         }
     }
-
-    @Override
-    public void onDestroyActionMode(ActionMode mode) {
-    }
-
 }
